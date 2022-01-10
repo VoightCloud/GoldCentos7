22506152bcd5ea643d367183f080a860e477e80b
Jeff Voight <commercial.voight@gmail.com>
2022-01-10 13:24:56 -0500

    hardcode index

--- template-ec2/outputs.tf
+++ template-ec2/outputs.tf
@@ -1,6 +1,6 @@
 output "ip" {
   # value = "192.168.137.201"
-  value = proxmox_vm_qemu.gold_build[proxmox_vm_qemu.gold_build.count.index].ipconfig0
+  value = proxmox_vm_qemu.gold_build[0].ipconfig0
 }
 
 output "vm_id" {
