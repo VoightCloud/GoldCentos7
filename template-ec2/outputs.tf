fc2c3c63c0c1b9b705060d388d189bb96d8be380
Jeff Voight <jeff@voight.org>
2022-01-15 22:17:06 -0500

    dynamic vmid

--- template-ec2/outputs.tf
+++ template-ec2/outputs.tf
@@ -1,10 +1,10 @@
 output "ip" {
-  value = "192.168.137.${var.vmid}"
+  value = "192.168.137.${proxmox_vm_qemu.gold_build[0].vmid}"
 #  value = proxmox_vm_qemu.gold_build[0].ipconfig0
 }
 
 output "vmid" {
-  value = var.vmid
+  value = proxmox_vm_qemu.gold_build[0].vmid
 }
 #
 #output "hostname" {
